99914e815dfae325ece0a12bc9e2ee9db7c64fd4
DanielSmelik <104142029+DanielSmelik@users.noreply.github.com>
2025-01-08 19:06:33 +0200

    Update tank_drive.py
    Added creation of motor examples.

--- tank_drive.py
+++ tank_drive.py
@@ -3,9 +3,14 @@ to make a turn, the wheels or tracks spin in opposite directions.
 By Daniel Smelik, Gvanim Robotics Lab, 2025.01.08.
 """
 
-
-import machine
 import time
 from neopixel import NeoPixel 
+#from ln298_motor_driver import motor  
 
-
+class TankDriveVehicle(motor1_pins, motor2_pins):
+  # A Tank Drive Vehicle is a Vehicle with 2 motors, without front wheel steering. This class gets 2 lists, where the first 2 elements are the digital pins and the last is the analog one.  
+  def __init__(self):
+    motorA = motor(motor1_pins)
+    motorB = motor(motor2_pins)
+  
+    
