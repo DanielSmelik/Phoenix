04466d7795165a86e05eb86c3dd10af3547eb41e
DanielSmelik <104142029+DanielSmelik@users.noreply.github.com>
2025-01-08 18:59:49 +0200

    Update tank_drive.py

--- tank_drive.py
+++ tank_drive.py
@@ -1,1 +1,11 @@
+"""A "Tank" driving system for vehicles with two motors. By "tank drive," I mean a driving system where,
+to make a turn, the wheels or tracks spin in opposite directions. 
+By Daniel Smelik, Gvanim Robotics Lab, 2025.01.08.
+"""
+
+
 import machine
+import time
+from neopixel import NeoPixel 
+
+
