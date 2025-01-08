bdbcdc37ec6f907b9a58b8bd28e086857434968f
DanielSmelik <104142029+DanielSmelik@users.noreply.github.com>
2025-01-08 19:07:05 +0200

    Update tank_drive.py

--- tank_drive.py
+++ tank_drive.py
@@ -5,7 +5,7 @@ By Daniel Smelik, Gvanim Robotics Lab, 2025.01.08.
 
 import time
 from neopixel import NeoPixel 
-#from ln298_motor_driver import motor  
+#from l298n_motor_driver import motor  
 
 class TankDriveVehicle(motor1_pins, motor2_pins):
   # A Tank Drive Vehicle is a Vehicle with 2 motors, without front wheel steering. This class gets 2 lists, where the first 2 elements are the digital pins and the last is the analog one.  
