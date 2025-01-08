f96da89e08b1c8d0bf7115c59ad3bef039956deb
DanielSmelik <104142029+DanielSmelik@users.noreply.github.com>
2025-01-08 22:40:47 +0200

    Update tank_drive.py
    Added driving functions.

--- tank_drive.py
+++ tank_drive.py
@@ -5,12 +5,25 @@ By Daniel Smelik, Gvanim Robotics Lab, 2025.01.08.
 
 import time
 from neopixel import NeoPixel 
-#from l298n_motor_driver import motor  
+from l298n_motor_driver import motor  
 
-class TankDriveVehicle(motor1_pins, motor2_pins):
+class TankDriveVehicle():
   # A Tank Drive Vehicle is a Vehicle with 2 motors, without front wheel steering. This class gets 2 lists, where the first 2 elements are the digital pins and the last is the analog one.  
-  def __init__(self):
-    motorA = motor(motor1_pins)
-    motorB = motor(motor2_pins)
-  
+  def __init__(self, motor1_pins, motor2_pins):
+    self.motorA = motor(motor1_pins[0], motor1_pins[1], motor1_pinds[2])
+    self.motorB = motor(motor2_pins[0], motor2_pins[1], motor2_pinds[2)
+
+  def go_forward(self, speed):
+    self.motorA.run("forward", speed)
+    self.motorB.run("forward", speed)
+  def go_backward(self, speed):
+    self.motorA.run("backward", speed)
+    self.motorB.run("backward", speed)
+  def go_right(self, speed):
+    self.motorA.run("forward", speed)
+    self.motorB.run("backward", speed)
+  def go_right(self, speed):
+    self.motorA.run("backward", speed)
+    self.motorB.run("forward", speed)
+      
     
