eaa16bc6dc7736b94555a7558439d798e3d87a38
DanielSmelik <104142029+DanielSmelik@users.noreply.github.com>
2025-01-08 21:31:30 +0200

    Update 298n_motor_driver.py
    Mistake fixes.

--- 298n_motor_driver.py
+++ 298n_motor_driver.py
@@ -11,18 +11,18 @@ Written by Daniel Smelik, 11th grade, 4th class, Gvanim Robotics Lab, 2025.01.08
 Feel free to use this code in all your projects. 
 """
 from machine import Pin, PWM
-from numpy import interp
+#from numpy import interp
 import time 
 
-class Motor(digital_pinA, digital_pinB, analog_pin):
-  def __init__(self):
+class Motor():
+  def __init__(self, digital_pinA, digital_pinB, analog_pin):
     self.digitalA = Pin(digital_pinA, Pin.OUT)
     self.digitalB = Pin(digital_pinB, Pin.OUT)
     self.analog = PWM(Pin(analog_pin))
   
-  def drive(dir, speed):
+  def drive(self, dir, speed):
     #dir should be "forward" or "backward". speed between 0 and 100. Forward is defined as the first pin being high, the second being low. 
-    mapped_speed = interp(speed, [0,100], [0, 1023]) # mapping the speed int for PWM values. 
+    #mapped_speed = interp(speed, [0,100], [0, 1023]) # mapping the speed int for PWM values. 
     
     if dir == "forward":
       self.digitalA.on()
@@ -30,6 +30,3 @@ class Motor(digital_pinA, digital_pinB, analog_pin):
       self.analog.duty(speed)
     print("Going Forward.")
       
-    
-    
-  
