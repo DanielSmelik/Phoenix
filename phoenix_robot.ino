66eac1ea7e21b9750ed839007fd8d560c1b09d50
yuval0978 <133561132+yuval0978@users.noreply.github.com>
2025-03-28 11:07:08 +0300

    Update phoenix_robot.ino

--- phoenix_robot.ino
+++ phoenix_robot.ino
@@ -1,13 +1,19 @@
 #include "clicli.h"
 #include "phoenix_robot.h"
+#include "navigator.h" 
 
 Phoenix robot(1);
 clicli mycli(robot);  
+navigator navigator(0);
 
 void setup() { 
   
   mycli.begin();
+  robot.begin();
+  navigator.begin();
+  navigator.gyro_value();
   
+
 
  }
 
