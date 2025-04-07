233bf6aa720ea4e818ab3b8991dfafc52bd960fc
yuval0978 <133561132+yuval0978@users.noreply.github.com>
2025-04-07 12:59:37 +0300

    Update navigator.h

--- navigator.h
+++ navigator.h
@@ -3,14 +3,23 @@
 
 class navigator{
     public:
-  navigator(int test_param);
+    navigator(int trig_1, int echo_1, int trig_2, int echo_2, int trig_3, int echo_3, int baz_pin);
     void begin();
     float gyro_value();
     float ultra_check(int trigPin, int echoPin);
     void run();
     void gyroturn(int sp, int times);
+    void reset_gyro();
+    void steer(int ang);
+    float check_us();
+    void go_to_dir();
     private:
-
+    int _trig_1, _trig_2, _trig_3;
+    int _echo_1, _echo_2, _echo_3;
+    int _baz_pin;
+    int us_front;
+    int us_left;
+    int us_right;
 
 };
 #endif 
