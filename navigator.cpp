c310e7c1b661a07ecc35363c8da894edf3329f89
yuval0978 <133561132+yuval0978@users.noreply.github.com>
2025-04-07 12:59:26 +0300

    Update navigator.cpp

--- navigator.cpp
+++ navigator.cpp
@@ -1,20 +1,38 @@
-#include "navigator.h" 
+#include "navigator.h"
 #include "Arduino.h"
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
- Adafruit_MPU6050 mpu;
-  
-  float angleZ = 0;
-  float gyroZoffset = 0;
-  unsigned long lastTime = 0;
-  unsigned long previous_time;
-  unsigned long current_time;
-  unsigned long elapsed_time;
-  float rate_error;
-navigator::navigator(int test_param){
-  //_test_param = test_param;
+Adafruit_MPU6050 mpu;
+
+float angleZ = 0;
+float gyroZoffset = 0;
+unsigned long lastTime = 0;
+unsigned long previous_time;
+unsigned long current_time;
+unsigned long elapsed_time;
+float rate_error;
+float out;
+float speedL;
+float speedR;
+float derivative;
+float pv;
+navigator::navigator(int trig_1, int echo_1, int trig_2, int echo_2, int trig_3, int echo_3, int baz_pin) {
+  _trig_1 = trig_1;
+  _trig_2 = trig_2;
+  _trig_3 = trig_3;
+  _echo_1 = echo_1;
+  _echo_2 = echo_2;
+  _echo_3 = echo_3;
+  _baz_pin = baz_pin;
+  pinMode(_trig_1, OUTPUT);
+  pinMode(_echo_1, INPUT);
+  pinMode(_trig_2, OUTPUT);
+  pinMode(_echo_2, INPUT);
+  pinMode(_trig_3, OUTPUT);
+  pinMode(_echo_3, INPUT);
+  pinMode(_baz_pin, OUTPUT);
 }
 
 void calibrateGyro() {
@@ -34,33 +52,26 @@ void calibrateGyro() {
 
 void navigator::begin() {
   Wire.begin();
-  Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
-    while (1);
+    while (1)
+      ;
   }
 
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
-  calibrateGyro(); // Perform calibration
+  calibrateGyro();  // Perform calibration
   lastTime = millis();
-  pinMode(2, OUTPUT); 
-  pinMode(15, INPUT);
-  pinMode(4, OUTPUT);
-  pinMode(0, INPUT);
-  pinMode(33, OUTPUT);
-  pinMode(32, INPUT);
   previous_time = micros();
-  
-
 }
 
 
-float navigator::gyro_value(){
+float navigator::gyro_value() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
   unsigned long currentTime = millis();
@@ -70,13 +81,18 @@ float navigator::gyro_value(){
   float gyroZ = g.gyro.z - gyroZoffset;
   // Apply a deadband to reduce drift when the sensor is stationary
   if (fabs(gyroZ) < 0.02) {  // threshold; adjust based on your observations
-  gyroZ = 0;
+    gyroZ = 0;
   }
   angleZ += gyroZ * dt * (180.0 / PI);
   return angleZ;
 }
 
-float navigator::ultra_check(int trigPin, int echoPin){
+void navigator::reset_gyro() {
+  angleZ = 0;  // Reset the angle to 0
+}
+
+
+float navigator::ultra_check(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -84,48 +100,89 @@ float navigator::ultra_check(int trigPin, int echoPin){
   digitalWrite(trigPin, LOW);
 
   float duration = pulseIn(echoPin, HIGH);
-  float distance = (duration*.0343)/2;
+  float distance = (duration * .0343) / 2;
   return distance;
 }
-  
 
-void navigator::run(){
-  int us_front = ultra_check(2, 15);
-  int us_left = ultra_check(4, 0);
-  int us_right = ultra_check(33, 32);  
+
+void navigator::run() {
+  int us_front = ultra_check(_trig_1, _echo_1);
+  int us_left = ultra_check(_trig_2, _echo_2);
+  int us_right = ultra_check(_trig_3, _echo_3);
 }
 
-void navigator::gyroturn(int sp, int times){
-  int KD = 2;
-  int KP = 4;
-  int KI = 0;
+void navigator::gyroturn(int sp, int times) {
+  float kd = 2;
+  float kp = 4;
+  float ki = 0;
   float cum_error = 0;
   float last_error = 0;
-  for (int i = 0; i < times; i++){
+  for (int i = 0; i < times; i++) {
     int pv = gyro_value();
     int error = sp - pv;
     current_time = micros();
     elapsed_time = current_time - previous_time;
     cum_error += error * elapsed_time;
-    if (elapsed_time == 0){
+    if (elapsed_time == 0) {
       rate_error = 0;  //Avoid division by zero
-    }
-    else{
+    } else {
       rate_error = (error - last_error) / elapsed_time;
     }
-    float out = KP * error + KI * cum_error + KD * rate_error;
-    if (out > 100);
-    out = 100;
-    if (out < 100);
-    out = 100;
-  
-
-
+    out = kp * error + ki * cum_error + kd * rate_error;
+    if (out > 100) {
+      out = 100;
+    }
+    if (out < 100) {
+      out = 100;
+    }
+    speedL = out;
+    speedR = -out;
+    //motor stuff
+    /*analogWrite(pin,speedL);
+    analogWrite(pin,speedR);
+    */
+    previous_time = current_time;
+    last_error = error;
+    reset_gyro();
   }
-
 }
 
-
-
-
-
+void navigator::steer(int ang) {
+  float kp = 4;
+  float ki = 0.1;
+  float kd = 2;
+  //def speed = 200;
+  float cum_error = 0;
+  float last_error = 0;
+  while (us_front > 20) {
+    pv = gyro_value();
+    float error = ang - pv;
+    current_time = micros();
+    elapsed_time = current_time - previous_time;
+    cum_error += error * elapsed_time;
+    if (elapsed_time == 0) {  // avoid dividing by zero
+      derivative = 0;
+    } else {
+      rate_error = (error - last_error) / elapsed_time;
+    }
+    out = kp * error + ki * cum_error + kd * rate_error;
+    if (out > 20) {
+      out = 20;
+    }
+    if (out < 20) {
+      out = 20;
+    }
+    /*
+    motor stuff
+    speedL = defspeed + out;
+    speedR = defspeed - out;    
+    analogWrite(pin,speedL);
+    analogWrite(pin,speedR);
+    */
+    previous_time = current_time;
+    last_error = error;
+  }
+  //analogWrite(pin,0);
+  //analogWrite(pin,0);
+  Serial.print('angle =', pv);
+}
