defab8d3a1659810acc43053d8b048f1a0c1b317
yuval0978 <133561132+yuval0978@users.noreply.github.com>
2025-04-07 17:02:48 +0300

    Update phoenix_robot.ino

--- phoenix_robot.ino
+++ phoenix_robot.ino
@@ -1,29 +1,32 @@
-#include "phoenix_robot.h"
-#include "navigator.h"
+  #include "phoenix_robot.h"
+  #include "navigator.h"
 
-Phoenix robot(1);
-navigator navigator(1,2,3,4,5,6,7);
+  Phoenix robot(1);
+  navigator navigator(1,2,3,4,5,6,7);
 
-int i = 0;
+  int i = 0;
 
-void setup() { 
-  Serial.begin(115200);
-  robot.begin();
-  navigator.begin();
+  void setup() { 
+    Serial.begin(115200);
+    robot.begin();
+    navigator.begin();
+    navigator.go_to_dir();
+  }
 
-}
-
-void loop() {
-  //robot.test(i);
-  //i++;
-  /*robot.motgo(100); 
-  delay(3000);
-  robot.motgo(0);
-  delay(3000);
-  */
-  robot.readcli();
-  delay(100);
-  navigator.run();
+  void loop() {
+    //robot.test(i);
+    //i++;
+    /*robot.motgo(100); 
+    delay(3000);
+    robot.motgo(0);
+    delay(3000);
+    */
+    robot.readcli();
+    delay(100);
+    navigator.run();
+    for (int i = 0; i < 100; i++){
+    navigator.go_to_dir();
+    }
   }
 
 
